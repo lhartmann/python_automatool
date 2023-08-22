36e831d4ecc008ddfd344d3bc06acc6583835839
Lucas Vinicius Hartmann <lucas.hartmann@gmail.com>
2023-08-21 23:34:06 -0300

    Add distribution files.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'automatool',
+  packages = ['automatool'],
+  version = '0.1',
+  license='MIT',
+  description = 'Automaton analysis and maniplation tool',
+  author = 'Lucas Vinicius Hartmann',
+  author_email = 'lucas.hartmann@gmail.com',
+  url = 'https://github.com/lhartmann/python_automatool',
+  download_url = 'https://github.com/lhartmann/python_automatool/archive/0.1.tar.gz',
+  keywords = ['discrete events', 'automaton', 'state machine'],
+  install_requires=[ 'copy', 'pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
